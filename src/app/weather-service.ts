51e8a0e07c80020ee2048a206901578b77b92f48
edumoro412 <edumoro2003@gmail.com>
2026-02-20 21:32:47 +0100

    feat: add WeatherService with methods for current weather, login, and registration

---
+++ src/app/weather-service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WeatherResponse } from './interfaces/weather';
+import {
+  LoginResponse,
+  RegisterResponse,
+  UserInterface,
+  UserRegisterInterface,
+} from './interfaces/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  private apiServerUrl = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {}
+
+  public getCurrent(lat: number, lon: number): Observable<WeatherResponse> {
+    return this.http.get<WeatherResponse>(
+      `${this.apiServerUrl}/weather/current`,
+      {
+        params: {
+          latitude: lat.toString(),
+          longitude: lon.toString(),
+        },
+      },
+    );
+  }
+
+  public login(user: UserInterface): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(
+      `${this.apiServerUrl}/auth/login`,
+      user,
+      {
+        responseType: 'text' as 'json',
+      },
+    );
+  }
+
+  public register(user: UserRegisterInterface): Observable<RegisterResponse> {
+    return this.http.post<RegisterResponse>(
+      `${this.apiServerUrl}/auth/register`,
+      user,
+    );
+  }
+}
