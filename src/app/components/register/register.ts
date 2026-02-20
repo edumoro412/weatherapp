aac3c3126db699bbaeb8863f6177c8b23ae87247
edumoro412 <edumoro2003@gmail.com>
2026-02-20 21:33:08 +0100

    feat: add registration component with form validation and error handling

---
+++ src/app/components/register/register.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { WeatherService } from '../../weather-service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RegisterResponse, UserRegisterInterface } from '../../interfaces/user';
+import { passwordValidator } from '../../validators/password.validator';
+
+@Component({
+  selector: 'app-register',
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './register.html',
+  styleUrl: './register.scss',
+})
+export class Register {
+  registerForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private weatherService: WeatherService,
+    private router: Router,
+  ) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, passwordValidator]],
+      name: ['', [Validators.required, Validators.minLength(2)]],
+    });
+  }
+
+  onSubmit() {
+    Object.keys(this.registerForm.controls).forEach(
+      (key) => this.registerForm.get(key)?.markAllAsTouched,
+    );
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    const userData: UserRegisterInterface = {
+      email: this.registerForm.value.email,
+      password: this.registerForm.value.password,
+      name: this.registerForm.value.name,
+    };
+
+    this.weatherService.register(userData).subscribe({
+      next: (response: RegisterResponse) => {
+        if (response.status === 201) {
+          this.router.navigate(['/login']);
+        }
+      },
+      error: (error: Error) => {
+        alert('Ocurrio un error ');
+      },
+    });
+  }
+}
