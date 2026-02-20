f39065446efdff667a9a7475b6b9eab583ca4fb9
edumoro412 <edumoro2003@gmail.com>
2026-02-20 21:32:59 +0100

    feat: implement login component with form validation and error handling

---
+++ src/app/components/login/login.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { WeatherService } from '../../weather-service';
+import { CommonModule } from '@angular/common';
+import { LoginResponse, UserInterface } from '../../interfaces/user';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private weatherService: WeatherService,
+    private router: Router,
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    Object.keys(this.loginForm.controls).forEach((key) =>
+      this.loginForm.get(key)?.markAllAsTouched(),
+    );
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const userData: UserInterface = {
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password,
+    };
+
+    console.log(userData, '=========?');
+
+    this.weatherService.login(userData).subscribe({
+      next: (response: LoginResponse) => {
+        if (typeof response === 'string') {
+          localStorage.setItem('token', response);
+        } else if (response && 'token' in response) {
+          localStorage.setItem('token', response.token);
+        }
+
+        this.router.navigate(['/']);
+      },
+      error: (error: Error) => {
+        console.error('Ocurrio un error', userData);
+      },
+    });
+  }
+}
