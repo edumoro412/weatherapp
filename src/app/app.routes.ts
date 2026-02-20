e5aba505080a699c32d94c81e14155e9e2cfa9ad
edumoro412 <edumoro2003@gmail.com>
2026-02-20 21:32:14 +0100

    feat(app):

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Authform } from './template/authform/authform';
+import { Register } from './components/register/register';
+import { Login } from './components/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Authform,
+    children: [
+      {
+        path: 'login',
+        component: Login,
+      },
+      {
+        path: 'register',
+        component: Register,
+      },
+    ],
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherService } from './weather-service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { WeatherResponse } from './interfaces/weather';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'weatherapp';
+
+  weatherResponse: WeatherResponse | undefined;
+
+  constructor(private weatherService: WeatherService) {}
 }
