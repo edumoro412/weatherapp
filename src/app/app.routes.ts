d79f65546ebb63410eed3fc7121b5826ca4a90e9
edumoro412 <edumoro2003@gmail.com>
2026-02-21 12:45:26 +0100

    feat(routes): added the AuthGuard but commented because it doesnt work

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Authform } from './template/authform/authform';
 import { Register } from './components/register/register';
 import { Login } from './components/login/login';
+/* import { AuthGuard } from './security/AuthGuard'; */
 
 export const routes: Routes = [
   {
     path: '',
     component: Authform,
+    /*     canActivate: [AuthGuard], */
     children: [
       {
         path: 'login',
